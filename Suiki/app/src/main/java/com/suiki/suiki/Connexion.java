d99c051c828b3f534c0a1ecec2138570cfdced9c
EsraKa <esra.karakus@ynov.com>
2016-11-30 15:53:05 +0100

    inscription

--- Suiki/app/src/main/java/com/suiki/suiki/Connexion.java
+++ Suiki/app/src/main/java/com/suiki/suiki/Connexion.java
@@ -1,7 +1,9 @@
 package com.suiki.suiki;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -10,12 +12,33 @@ import android.widget.Button;
 
 public class Connexion extends Activity{
 
-  private Button connexion;
-  private Button inscription;
+  private Button connexion = null;
+  private Button inscription = null;
+  private Intent intent = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.connexion);
+
+
+    this.connexion = (Button)findViewById(R.id.connexion);
+    this.inscription = (Button)findViewById(R.id.inscription);
+
+    this.connexion.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View v) {
+        intent = new Intent(Connexion.this , Profile.class);
+        startActivity(intent);
+      }
+    });
+
+    this.inscription.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View v) {
+        intent = new Intent(Connexion.this , Inscription.class);
+        startActivity(intent);
+      }
+    });
   }
 }
---
+++ Suiki/app/src/main/java/com/suiki/suiki/Inscription.java
@@ -0,0 +1,17 @@
+package com.suiki.suiki;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by Esra on 30/11/2016.
+ */
+
+public class Inscription extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.inscription);
+  }
+}
