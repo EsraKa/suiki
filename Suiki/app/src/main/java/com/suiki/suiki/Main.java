3d455b7889b0e4917de1129e4b8518371ae4894c
EsraKa <esra.karakus@ynov.com>
2016-11-30 12:13:41 +0100

    Main.xml

--- Suiki/app/src/main/java/com/suiki/suiki/Main.java
+++ Suiki/app/src/main/java/com/suiki/suiki/Main.java
@@ -2,12 +2,19 @@ package com.suiki.suiki;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
+
+  private Button connexion;
+  private Button inscription;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
   }
+
+
+
 }
