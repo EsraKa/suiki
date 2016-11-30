2f9449fab27732df45dda47d05947ae9e0c11b62
Maxxil <massil.kadi@ynov.com>
2016-11-30 13:46:09 +0100

    TEST MENU

--- Suiki/app/src/main/java/com/suiki/suiki/Main.java
+++ Suiki/app/src/main/java/com/suiki/suiki/Main.java
@@ -2,6 +2,10 @@ package com.suiki.suiki;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
 
 public class Main extends AppCompatActivity {
@@ -15,6 +19,11 @@ public class Main extends AppCompatActivity {
     setContentView(R.layout.main);
   }
 
-
-
+ //@Override
+  /*public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.slide , menu);
+    return true;
+  }*/
 }
