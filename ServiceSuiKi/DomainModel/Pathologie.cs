6bcb839ff83554cc3a9581b67abd4f13cbfbacf0
Maxxil <massil.kadi@ynov.com>
2016-12-14 15:49:10 +0100

    MAJ Service

--- ServiceSuiKi/DomainModel/Pathologie.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DomainModel
-{
-    public class Pathologie
-    {
-        public int Id_Pathologie { get; set; }
-        public string Nom { get; set; }
-        public string Description{ get; set; } 
-    }
-}
--- ServiceSuiKi/WebApplication1/Controllers/IndexController.cs
+++ ServiceSuiKi/WebApplication1/Controllers/IndexController.cs
@@ -4,18 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Dal;
-using DomainModel;
 
 namespace WebApplication1.Controllers
 {
     public class IndexController : ApiController
     {
         // GET: api/Index
-        public Personne Get()
-        {            
-            Context context = new Context();
-            return context.Personnes.FirstOrDefault(x => x.Id_Perso == 1);
+        public void Get()
+        {
+       
         }
 
         // GET: api/Index/5
