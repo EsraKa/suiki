f5af10f135b7bb9cf90f90a23f380b674e971f8c
Oloick <loickode@hotmail.fr>
2017-01-11 15:47:58 +0100

    Service projet Solve bug

---
+++ Service Suiki/BLL/PersonneBll.cs
@@ -0,0 +1,37 @@
+﻿using Dal;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PersonneBll
+    {
+        public personne GetPersonne(int id)
+        {
+            using (PersonneDal ctx = new PersonneDal())
+            {
+                return ctx.GetPersonne(id);
+            }
+        }
+
+        public List<personne> GetPersonne()
+        {
+            using (PersonneDal ctx = new PersonneDal())
+            {
+                return ctx.GetPersonne();
+            }
+        }
+
+        public static void AddPersonne(personne personne)
+        {
+            using (PersonneDal ctx = new PersonneDal())
+            {
+                ctx.AddPersonne(personne);
+            }
+        }
+    }
+}
