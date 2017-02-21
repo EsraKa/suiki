3badb607c5c77a01abb195fde4736a3d0ba75142
oloick <loickode@hotmail.com>
2017-02-21 12:03:26 +0100

    patientController

--- Service/controller/patientController.js
+++ Service/controller/patientController.js
@@ -1,90 +1,51 @@
-var router = require('express').Router();
-var mongoose = require('mongoose');
-var Utilisateur = require('./../model/suiki/utilisateur');
-var Personne = require('./../model/suiki/patient');
-var Medecin = require('./../model/suiki/medecin');
-var ReponseHttp = require('./../model/http/httpReponse');
-
+/**
+ * Created by Massil on 18/02/2017.
+ */
+var rooter = require('express').Router();
 var bodyParser = require('body-parser');
 
-router.use(bodyParser.json());
+var Patient = require('./../model/suiki/patient');
+var Medecin = require('./../model/suiki/medecin');
 
-/**
- * Connexion d'un utilisateur patient
- * Renvoie true si connexion réussie et Profile de l'utilisateur
- */
-router.post('/patient' , function (req , res) {
-    console.log(req.body);
-    var nom_utilisateur = req.body.nom_utilisateur;
-    var password = req.body.mot_de_passe;
 
-    connecter(nom_utilisateur , password, res);
+//rooter.use(bodyParser.json());
+
+rooter.get('/:idPatient/patient' , function (req , res) {
+    var idPatient = req.params.idPatient;
+
+
 
 });
 
-/**
- * Connexion d'un utilisateur medecin
- * Renvoie true si connexion réussie et liste des patients
- */
-router.post('/medecin' , function (req , res) {
-    console.log(req.body);
-    var nom_utilisateur = req.body.nom_utilisateur;
-    var password = req.body.mot_de_passe;
-
-    connecterMedecin(nom_utilisateur , password, res);
+rooter.post('/:idMedecin/medecin', function (req, res) {
+    var idMedecin = req.params.idMedecin;
+    console.log(idMedecin);
+    getMedecin(idMedecin.toString(),res);
 });
 
-/**
- * Fonction permettant la connexion à l'application pour un utilisateur
- * @param nom_utilisateur   Nom d'utilisateur
- * @param password          Mot de passe de l'utilisateur
- * @param res               Resultat
- */
-var connecter = function(nom_utilisateur , password , res)
+
+var getPatient = function(patientId , res)
 {
-    Utilisateur
-        .findOne({nom_utilisateur : nom_utilisateur , mot_de_passe: password})
-        .populate('profile')
-        .exec(function(err , user) {
-            var reponse = ReponseHttp.setReponse(true , user.profile , err);
-            res.send(reponse);
+    Patient
+        .find({personne : patientId})
+        .exec(function(err , patient){
+            res.send();
+        })
+};
+
+var getMedecin = function (idMedecin, res) {
+    Medecin
+        .findById(idMedecin)
+        .populate("personne")
+        .exec(function (err , data) {
+            console.log("medecin information (getMedecin) : " + data);
+            console.log(err);
+            res.send({medecin: data});
             res.end();
         });
+    /* .exec(function (err , medecin) {
+
+     });*/
 };
 
-
-/**
- * Fonction permettant la connexion à l'application pour un medecin
- * @param nom_utilisateur   Nom d'utilisateur
- * @param password          Mot de passe de l'utilisateur
- * @param res               Resultat
- */
-var connecterMedecin = function(nom_utilisateur , password , res)
-{
-    Utilisateur
-        .findOne({nom_utilisateur : nom_utilisateur , mot_de_passe: password})
-        .populate('profile')
-        .exec(function(err , user) {
-            verificationMedecin(user.profile , res);
-        });
-};
-
-/**
- * Fonction vérifiant qu'une personne est bien un médecin
- * @param personneM         La personne à vérifier
- * @param res               Le résultat de la requête
- */
-var verificationMedecin = function(personneM , res)
-{
-    console.log(personneM.nom +" Personne dans vérification Medecin \n\n");
-    Medecin
-        .find({personne: personneM._id})
-        .populate('personne')
-        .populate('patient')
-        .exec(function(err, medecin) {
-            console.log("Information medecin" + medecin);
-            res.send({status : true , medecin : medecin , patient : medecin, error : err});
-        });
-};
-
-module.exports = router;
+module.exports = rooter;
